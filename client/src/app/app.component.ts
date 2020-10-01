d61da8af7f219ebebb1105d219a173fc8f203f8a
Szilvia <szilviakovacs55@gmail.com>
2020-10-01 20:12:32 +0300

    angular ui added to project

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component,OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Flight reservation';
+  users: any;
+
+  ngOnInit(): void{
+    //this.getUsers();
+  }
+
+}
+
+
+/*
+  constructor(private httpClient: HttpClient){
+  }
+
+ 
+
+  getUsers(): void {
+    this.httpClient.get(
+    'http://localhost:5001/api/users').
+    subscribe(
+      response => {this.users = response; },
+       error => {console.log(error); });
+  }*/
