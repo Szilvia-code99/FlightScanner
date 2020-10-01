55bbee0072e3c073709187e40b2682ba48a3bde3
Szilvia <szilviakovacs55@gmail.com>
2020-10-01 18:45:25 +0300

    first commit

---
+++ API/Entities/User.cs
@@ -0,0 +1,12 @@
+namespace API.Entities
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string name { get; set; }
+
+        //public string password { get; set; }
+
+        
+    }
+}
