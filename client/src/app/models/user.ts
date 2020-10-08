572cab1c6a2e8522b8916666de2cf22a47945d12
Szilvia <szilviakovacs55@gmail.com>
2020-10-08 23:03:18 +0300

    register works with validations

--- client/src/app/models/user.ts
+++ client/src/app/models/user.ts
@@ -3,8 +3,8 @@ import { NgbDate } from '@ng-bootstrap/ng-bootstrap';
 export interface User {
    userName: string;
    token: string;
-  /* firstName: string;
+   firstName: string;
    lastName: string;
    dateOfBirth: Date;
-   idNumber: number;*/
+   idNumber: number;
 }
